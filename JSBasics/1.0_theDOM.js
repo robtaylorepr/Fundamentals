310094242fcc98680464698e34d5419bf493687d
admin <admin@admins-MacBook-Pro.local>
2016-09-22 08:26:12 -0700

    DOM

---
+++ JSBasics/1.0_theDOM.js
@@ -0,0 +1,14 @@
+//document.onLoad(alert("HEY HEY HEY!!!"));
+//console.log ("look at me...we are working together");
+
+
+var it  = document.getElementById("music");
+var foo = document.getElementById("best");
+console.log (foo[0].innerHTML;
+
+function favoriteSong(){
+	document.getElementById('favorite').innerHTML = 'freebird!!!';
+}
+	favoriteSong();
+
+
